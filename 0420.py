c91aa0c03fe9b86bc17dfcc24969a4b82a3faf65
SSeohy <seohh.jun@gmail.com>
2026-04-20 18:55:28 +0900

    refactor: 파일면 Untitled-1.py > 0420.py 변경 및 내용 수정

--- 0420.py
+++ 0420.py
@@ -4,3 +4,6 @@ string = "진짜 왜이러는지 모르겠는데/이제 알았음 문자열은 s
 print(string)
 print("여러 줄 주석 처리 방법: visual studio code 에서는 주석 처리 하고 싶은 부분을 드래그 ctrl / 하면 앞에 #이 붙고, " \
 "앞에 #이 붙은 줄은 전부 주석 처리 됨")
+
+string2 = "branch2인 text/explain 실험"
+print(string2, "왜 안될까....")
