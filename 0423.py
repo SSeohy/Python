7578e8a709319e787b43f7d18375e3d1a5c2348e
SSeohy <seohh.jun@gmail.com>
2026-04-23 17:23:47 +0900

    unit3

--- 0423.py
+++ 0423.py
@@ -1,2 +1,13 @@
 #Unit3. Hello, world!로 시작하기
 
+print('hello python!')
+#파이썬은 대소문자 구분. NameError: name ...is not defined > 대소문자 에러
+#파이썬 셸(Shell): IDLE처럼 파이썬 코드를 직접 입력해서 실행하는 프로그램.
+#   >>>: 파이썬 프롬프르(prompt). 파이썬 셸을 통해 파이썬 코드를 해석(interpret)해주는 인터프리터.
+
+print('hello python!') #print()는 함수. print는 함수명, ()안에 들어가는 내용을 실행(호출)한다.
+# [명령 프롬프트에서 .py 스크립트 파일 실행하기]
+# Win R 키로 cmd 입력 > 명령 프롬프트 열기
+# 해당 파일이 있는 경로로 이동하여 python 파일명.py 입력 > 실행됨
+
+
